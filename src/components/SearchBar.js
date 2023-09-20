86048f227cda749e3446004d3d7bcfde6b44df6a
Ayesha <ayeshaiqbal2592@gmail.com>
2023-09-20 10:34:56 +0500

    Progressed on Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import { useToggle } from "../context/ToggleContext";
+import { db } from "../firebase/firebaseConfig";
+import { collection, getDocs, query, where, or } from "firebase/firestore";
+
 export const SearchBar = () => {
+
+  const { toggleState } = useToggle();
+
+  const [searchVal, setSearchVal] = useState("");
+
+  function handleChange(event) {
+    setSearchVal(event.target.value)
+  };
+
+  async function fetchDocs(searchQ){
+    const collectionRef = collection(db, "Notes")
+    const queryRef = query(collectionRef,  where('title', '==', searchQ));
+    try {
+      // Fetch Documents
+    } catch (error) {
+      console.error('Error fetching documents:', error);
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const searchQuery = searchVal;
+    if (searchQuery !== "") {
+      fetchDocs(searchQuery);
+    }
+  };
+  
   return (
     <div>
-        <form className="hidden md:flex mb-20 justify-center">
-            <input type="text" placeholder="Search by a username, title, or keyword"
+        <form onSubmit={handleSubmit} className="hidden md:flex mb-20 justify-center">
+            <input type="text" onChange={handleChange} value={searchVal} placeholder="Search by a username, title, or keyword"
             className="w-in-w px-4 py-2.5 rounded-xl border border-gray-300 shadow-lg focus:outline-notePink"/>
         </form>
     </div>
